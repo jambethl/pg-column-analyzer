36025a4fb85845592c849b7b8b7bd45c3775a9c5
James Lambeth <jambethl@gmail.com>
2024-06-06 18:08:59 +0100

    add data type size map

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -9,6 +9,21 @@ import (
 	"strconv"
 )
 
+var dataTypeMap = map[string]string{
+	"smallint":                    "2 bytes",
+	"integer":                     "4 bytes",
+	"bigint":                      "8 bytes",
+	"boolean":                     "1 bytes",
+	"real":                        "4 bytes",
+	"double precision":            "8 bytes",
+	"date":                        "4 bytes",
+	"timestamp without time zone": "8 bytes",
+	"timestamp with time zone":    "8 bytes",
+	"text":                        "10 bytes", // Variable size, often larger
+	"varchar":                     "10 bytes", // Variable size, often larger
+	"bytea":                       "10 bytes", // Variable size, often larger
+}
+
 func GenerateReport(columnList []types.ColumnInfo, tableName string) error {
 	reportName := fmt.Sprintf("reports/%s_report.csv", tableName)
 	file, err := os.Create(reportName)
@@ -21,7 +36,7 @@ func GenerateReport(columnList []types.ColumnInfo, tableName string) error {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	writer.Write([]string{"Ordinal Position", "Column Name", "Data Type", "Nullable"})
+	writer.Write([]string{"Ordinal Position", "Column Name", "Data Type", "Nullable", "Data Type Size (B)", "Wasted Padding", "Recommended Position"})
 
 	for _, col := range columnList {
 		writer.Write([]string{
@@ -29,6 +44,7 @@ func GenerateReport(columnList []types.ColumnInfo, tableName string) error {
 			col.ColumnName,
 			col.DataType,
 			col.IsNullable,
+			dataTypeMap[col.DataType],
 		})
 	}
 
