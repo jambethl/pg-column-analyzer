c22fad1ccee49b0463a4f18297c78077e9e65973
James Lambeth <jambethl@gmail.com>
2024-06-26 18:25:41 +0100

    improve db error handling

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -17,5 +17,15 @@ type Config struct {
 
 func Connect(config Config) (*sql.DB, error) {
 	connStr := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable", config.Host, config.DBName, config.UserName, config.Password)
-	return sql.Open("postgres", connStr)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
 }
